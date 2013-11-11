f00b78639d14d96f36a62cd239de160ebf90ea39
jimmytheleaf <jim.fingal@gmail.com>
2013-11-11 13:39:41 -0800

    Pushing what I have now, enabling / disabling movement still doesn't quite work.

---
+++ Assets/MovementControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementControl : MonoBehaviour {
+
+	bool can_move = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Standard Assets/Character Controllers/Sources/Scripts/CharacterMotor.js
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/CharacterMotor.js
@@ -33,7 +33,7 @@ class CharacterMotorMovement {
 	var maxAirAcceleration : float = 20.0;
 
 	// The gravity for the character
-	var gravity : float = 10.0;
+	var gravity : float = 30.0;
 	var maxFallSpeed : float = 20.0;
 	
 	// For the next variables, @System.NonSerialized tells Unity to not serialize the variable or show it in the inspector view.
