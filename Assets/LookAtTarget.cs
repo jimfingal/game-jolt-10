2c20d4dccad26d4f117064bb09bbc47640dcc58d
dynne <christa.hartsock@gmail.com>
2013-11-10 21:57:53 -0800

    initial commit

---
+++ Assets/LookAtTarget.cs
@@ -0,0 +1,16 @@
+﻿// C#
+using System;
+using UnityEngine;
+
+public class LookAtTarget : MonoBehaviour
+{
+	public Transform target;
+	
+	void Update()
+	{
+		if(target != null)
+		{
+			transform.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z));
+		}
+	}
+}
