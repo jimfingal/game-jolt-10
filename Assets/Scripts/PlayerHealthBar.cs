2263f129a8a07a5ad249c9dbb96c19cf82afdbb8
jimmytheleaf <jim.fingal@gmail.com>
2013-11-12 12:27:15 -0800

    Dialog Controller, health bars

---
+++ Assets/Scripts/PlayerHealthBar.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealthBar : MonoBehaviour {
+
+	float totalHealth = 100;
+	float currentHealth = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public float getHealthPercentage() {
+		return currentHealth / totalHealth;
+	}
+}
---
+++ Assets/Scripts/PlayerHealthBarDisplay.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealthBarDisplay : MonoBehaviour
+{
+
+	//current progress
+	public float barDisplay;
+
+
+	public int boxPosX = 10;
+	public int boxPosY = 500;
+
+	public int boxSizeX = 250;
+	public int boxSizeY = 50;
+
+	private Vector2 pos;
+	private Vector2 size;
+
+	private PlayerHealthBar playerHealthBar;
+
+	Texture progressBackground;	
+	Texture progressForground;
+
+	void OnGUI()
+	{
+		//draw the background:
+
+		GUI.Box(new Rect(pos.x, pos.y, size.x, size.y), progressBackground);
+		GUI.Box(new Rect(pos.x, pos.y, playerHealthBar.getHealthPercentage() * size.x, size.y), progressForground);
+	}
+	
+	void Update()
+	{
+		//the player's health
+		barDisplay = this.playerHealthBar.getHealthPercentage();
+	}
+
+	void Start() 
+	{
+		this.playerHealthBar = this.gameObject.GetComponent<PlayerHealthBar>();
+
+		this.pos = new Vector2(10,500);
+		this.size = new Vector2(250,50);
+	}
+}
