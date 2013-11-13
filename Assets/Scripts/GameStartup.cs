560fc7de3199e6a48157d622d9fc640456a4e801
jimmytheleaf <jim.fingal@gmail.com>
2013-11-13 15:51:43 -0800

    Startup in proper resolution; fix audio parameters

--- Assets/Scripts/GameStartup.cs
+++ Assets/Scripts/GameStartup.cs
@@ -5,7 +5,10 @@ public class GameStartup : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Screen.SetResolution (960, 600, false, 60);
+
+		if (Application.isWebPlayer) {
+			Screen.SetResolution (960, 600, false, 60);
+		}
 	}
 
 }
