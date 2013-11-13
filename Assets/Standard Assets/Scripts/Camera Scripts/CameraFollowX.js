5807bf9c95f9ebb871c38e5edfa2d2956c4df8b1
dynne <christa.hartsock@gmail.com>
2013-11-12 16:30:24 -0800

    New Scene

---
+++ Assets/Standard Assets/Scripts/Camera Scripts/CameraFollowX.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var target : Transform;
+var CameraDistance : int=0;
+var CameraHeight : int=0;
+
+function Start () {
+
+}
+
+function Update () 
+{
+	transform.position = Vector3(-CameraDistance, CameraHeight, target.transform.position.z);
+}
