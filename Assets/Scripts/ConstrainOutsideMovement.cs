a5c71cd83b1bfd03291e077aa1b93d3ab224e8e9
jimmytheleaf <jim.fingal@gmail.com>
2013-11-13 14:04:48 -0800

    Constrain rotation; upgrade to non-beta version of unity4.3

--- Assets/Scripts/ConstrainOutsideMovement.cs
+++ Assets/Scripts/ConstrainOutsideMovement.cs
@@ -9,16 +9,20 @@ public class ConstrainOutsideMovement : MonoBehaviour {
 	private float initialYRotation;
 
 
+	private Quaternion initialRotation;
+
+
 	void Start () {
 		initialXPosition = transform.position.x;
 		initialYRotation = transform.rotation.y;
+		initialRotation = transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 		transform.position = new Vector3(initialXPosition ,transform.position.y, transform.position.z);
-		//transform.rotation = new Vector3(rotation.x,rotations.y,0);
+		transform.rotation = initialRotation;
 
 	}
 }
