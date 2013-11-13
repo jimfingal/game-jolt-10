b6d3e5be5010c02a0acaaf1bafbbe2465a3dde2a
jimmytheleaf <jim.fingal@gmail.com>
2013-11-13 13:45:05 -0800

    Initial script to constrain outside movements; colliders on pathway edges

---
+++ Assets/Scripts/ConstrainOutsideMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstrainOutsideMovement : MonoBehaviour {
+
+	// Use this for initialization
+
+	private float initialXPosition;
+	private float initialYRotation;
+
+
+	void Start () {
+		initialXPosition = transform.position.x;
+		initialYRotation = transform.rotation.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = new Vector3(initialXPosition ,transform.position.y, transform.position.z);
+		//transform.rotation = new Vector3(rotation.x,rotations.y,0);
+
+	}
+}
--- Assets/Scripts/ConstrainXYMovement.js
+++
@@ -1,6 +0,0 @@
-﻿#pragma strict
-//Constrain Player Movement to Z Axis
-function Update () {
-    transform.position.y = 0;
-    transform.position.x = 0;
-}
