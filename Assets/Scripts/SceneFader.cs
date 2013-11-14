023dc41614ffb65034dc9fe875123bc2a3a30c0f
jimmytheleaf <jim.fingal@gmail.com>
2013-11-13 19:13:13 -0800

    Fade in Audio; Draw level for GUI elements

--- Assets/Scripts/SceneFader.cs
+++ Assets/Scripts/SceneFader.cs
@@ -10,14 +10,15 @@ public class SceneFader : MonoBehaviour {
 	private float startTime;
 
 	private string loadThisLevel;
-	private bool fadeOut = false;
 	private float fadeOutStart;
+
+	public bool fadeIn = true;
+	private bool fadeOut = false;
+
 
 	// Use this for initialization
 	void Start () {
 		startTime = Time.time;
-
-		// fadeIn();
 	}
 	
 	// Update is called once per frame
@@ -51,7 +52,7 @@ public class SceneFader : MonoBehaviour {
 	}
 
 	private bool inFadeIn() {
-		return Time.time - startTime < fadeLength;
+		return fadeIn && (Time.time - startTime < fadeLength);
 	}
 
 	
@@ -62,9 +63,13 @@ public class SceneFader : MonoBehaviour {
 	private float drawFadeIn() {
 		float percentageDone = (Time.time - startTime) / fadeLength;
 		float alphaBlend = (255 - (255 * percentageDone)) / 255;
-		
+
+		GUI.depth = 0;
 		GUI.color = new Color(63, 63, 63, alphaBlend);
 		GUI.DrawTexture(new Rect( 0, 0, Screen.width, Screen.height ), this.blankScreenTexture, ScaleMode.StretchToFill, true);
+		GUI.depth = 1;
+		AudioListener.volume = percentageDone;
+
 		return percentageDone;
 	}
 
@@ -75,6 +80,8 @@ public class SceneFader : MonoBehaviour {
 
 		GUI.color = new Color(63, 63, 63, alphaBlend);
 		GUI.DrawTexture(new Rect( 0, 0, Screen.width, Screen.height ), this.blankScreenTexture, ScaleMode.StretchToFill, true);
+
+		AudioListener.volume = percentageDone;
 
 		return percentageDone;
 	}
