86924d158b86fcdfc464c739cb1062b3a8d548b8
jimmytheleaf <jim.fingal@gmail.com>
2013-11-14 12:29:59 -0800

    Endgame zoomout

--- Assets/Scripts/CameraEndZoom.cs
+++ Assets/Scripts/CameraEndZoom.cs
@@ -5,37 +5,41 @@ public class CameraEndZoom : MonoBehaviour {
 
 	public GameObject player;
 	public float playerDistance = 1200;
-	public float cameraHeight = 40;
-
+	public float playerTargetZ = 1100;
+	
 	public Vector3 initialPosition;
 	public Vector3 targetPosition;
-
-	public float initialX = 65;
-	public float targetX = -500;
-
-	public float initialZ = 1100;
-	public float targetZ = 540;
-	public float playerTargetZ = 1100;
 
 	public float playerDistanceAlongTrack;
 
 	public Vector3 targetFocus;
 	public Vector3 endFocus;
 
+	public float initialFieldOfView;
+	public float targetFieldOfView;
+
+	private Camera camera;
+
 	// Use this for initialization
 	void Start () {
+
+		camera = gameObject.GetComponent<Camera>();
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		playerDistanceAlongTrack = 1 - ((playerTargetZ - player.transform.position.z) / 1200);
+		playerDistanceAlongTrack = 1 - ((playerTargetZ - player.transform.position.z) / playerDistance);
 
 		transform.position = initialPosition + (targetPosition - initialPosition) * playerDistanceAlongTrack;
 
 
-		transform.LookAt(targetFocus);
-	
+		transform.LookAt(targetFocus - (targetFocus - endFocus) * playerDistanceAlongTrack );
+
+		camera.fieldOfView = initialFieldOfView + (targetFieldOfView - initialFieldOfView)  * playerDistanceAlongTrack;
+		if (camera.fieldOfView > 170) {
+			camera.fieldOfView = 170;
+		}
 	}
 }
