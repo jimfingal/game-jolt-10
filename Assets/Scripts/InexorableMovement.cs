2b42da372f671de66d3a4a0648689607a27f1013
jimmytheleaf <jim.fingal@gmail.com>
2013-11-14 12:09:24 -0800

    Some outside stuff

---
+++ Assets/Scripts/InexorableMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InexorableMovement : MonoBehaviour {
+
+	public GameObject movable;
+
+	public Vector3 speed;
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") !=0) {
+			movable.transform.Translate(speed * Time.deltaTime, Space.World);
+		}
+	}
+}
---
+++ Assets/Scripts/LevelChangingTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelChangingTrigger : MonoBehaviour {
+
+	public string levelToTransitionTo;
+
+	private SceneFader fader;
+
+
+	// Use this for initialization
+	void Start () {
+
+		this.fader = gameObject.GetComponent<SceneFader>();
+
+	}
+	
+
+	void OnTriggerEnter (Collider other) {
+		this.fader.triggerFadeOut(levelToTransitionTo);
+	}
+
+
+}
+
+	
